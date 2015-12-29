bbdc07325cfae1775231e83459253fad990213d8
vlad.nedelcu@gmail.com <vlad.nedelcu@gmail.com>
2015-12-29 11:54:36 +0200

    added tabindex option

--- ReCaptcha.php
+++ ReCaptcha.php
@@ -66,7 +66,10 @@ class ReCaptcha extends InputWidget
     /** @var string The size of the widget. [[SIZE_NORMAL]] (default) or [[SIZE_COMPACT]] */
     public $size;
 
-    /** @var string Your JS callback function that's executed when the user submits a successful CAPTCHA response. */
+    /** @var int The tabindex of the widget  */
+    public $tabindex;
+
+    /** @var string Your JS callback function that's executed when the recaptcha response expires and the user needs to solve a new CAPTCHA. */
     public $jsCallback;
 
     /** @var array Additional html widget options, such as `class`. */
@@ -105,6 +108,9 @@ class ReCaptcha extends InputWidget
         }
         if (!empty($this->size)) {
             $divOptions['data-size'] = $this->size;
+        }
+        if (!empty($this->tabindex)) {
+            $divOptions['data-tabindex'] = $this->tabindex;
         }
 
         if (isset($this->widgetOptions['class'])) {
