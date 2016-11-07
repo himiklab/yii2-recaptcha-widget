9b69b9eb84d144a630d3b6d7b96b2aadeb893912
Aleksandr <nixomsk@gmail.com>
2016-11-07 12:30:57 +0600

    Fix for php 7
    Function empty() in php 7 don't create reCaptcha module instance on call empty(Yii::$app->reCaptcha->secret) its just return false if module instance don't exist yet. Yii::$app->get() creates module instance if its don' exist and return its.

--- ReCaptchaValidator.php
+++ ReCaptchaValidator.php
@@ -42,7 +42,7 @@ class ReCaptchaValidator extends Validator
     {
         parent::init();
         if (empty($this->secret)) {
-            if (!empty(Yii::$app->reCaptcha->secret)) {
+            if (!empty(Yii::$app->get('reCaptcha')->secret)) {
                 $this->secret = Yii::$app->reCaptcha->secret;
             } else {
                 throw new InvalidConfigException('Required `secret` param isn\'t set.');
